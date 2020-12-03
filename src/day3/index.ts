526384ce56cf6ad07bb61f648ddaa0bf26b3480b
Maor Leger <malege@microsoft.com>
2020-12-02 21:20:42 -0800

    cleaned up

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -1,41 +1,41 @@
-import { test, readInput } from "../utils/index"
+import { readInput } from "../utils/index";
 
-const prepareInput = (rawInput: string) => rawInput.split("\r\n").map(line => line.split(""))
+const prepareInput = (rawInput: string): string[][] =>
+  rawInput.split("\r\n").map((line) => line.split(""));
 
-const input = prepareInput(readInput())
+const input = prepareInput(readInput());
 
-const goA = (input, right, down) => {
-  let row = 0
-  let col = 0
+const goA = (input: string[][], right: number, down: number) => {
+  let row = 0;
+  let col = 0;
   let count = 0;
 
   while (row < input.length) {
     if (input[row][col] == "#") {
-      count++
+      count++;
     }
-    console.log(row, col)
-    col = (col + right) % input[row].length
 
-    row += down
+    col = (col + right) % input[row].length;
+    row += down;
   }
-  return count
-}
+  return count;
+};
 
-const goB = (input) => {
-  return goA(input, 1, 1) * goA(input, 3, 1) * goA(input, 5, 1) * goA(input, 7, 1) * goA(input, 1, 2)
-}
-
-
-/* Tests */
-
-test(1, 1)
+const goB = (input: string[][]) =>
+  [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+  ].reduce((acc, [right, down]) => acc * goA(input, right, down), 1);
 
 /* Results */
 
-console.time("Time")
-const resultA = goA(input, 3, 1)
-const resultB = goB(input)
-console.timeEnd("Time")
+console.time("Time");
+const resultA = goA(input, 3, 1);
+const resultB = goB(input);
+console.timeEnd("Time");
 
-console.log("Solution to part 1:", resultA)
-console.log("Solution to part 2:", resultB)
+console.log("Solution to part 1:", resultA);
+console.log("Solution to part 2:", resultB);
