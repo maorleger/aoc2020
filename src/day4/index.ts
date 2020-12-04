679fc9a0c42cc23eb734c32d997b1b18dbca7458
Maor Leger <malege@microsoft.com>
2020-12-04 09:38:30 -0800

    cleanup

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -1,23 +1,15 @@
-import { test, readInput } from "../utils/index"
+import { test, readInput } from "../utils/index";
 
-const prepareInput = (rawInput: string) => { 
-  var pairs = rawInput.split("\r\n\r\n").map(chunk => chunk.split("\r\n").flatMap(row => row.split(" ")))
-  var result = []
-  for (let set of pairs) {
-    var obj = {}
-    for (let line of set) {
-      let [k, v] = line.split(":");
-      obj[k] = v
-    }
+const prepareInput = (rawInput: string) => {
+  return rawInput.split("\r\n\r\n").map((record) => {
+    let pairs = record.split(/\r\n| /g).map((pair) => pair.split(":"));
+    return Object.fromEntries(pairs);
+  });
+};
 
-    result.push(obj)
-  }
-  return result
-}
+const input = prepareInput(readInput());
 
-const input = prepareInput(readInput())
-
-const goA = (input) => {
+const goA = (input: { [key: string]: string }[]) => {
   return input.filter(
     (record) =>
       Object.keys(record).length === 8 ||
@@ -25,45 +17,45 @@ const goA = (input) => {
   ).length;
 };
 
-const goB = (input) => {
+const goB = (input: { [key: string]: string}[]) => {
   let rules = {
-    "byr": (byr) => byr.length === 4 && parseInt(byr) >= 1920 && parseInt(byr) <= 2002,
-    "iyr": (iyr) => iyr.length === 4 && parseInt(iyr) >= 2010 && parseInt(iyr) <= 2020,
-    "eyr": (eyr) => eyr.length === 4 && parseInt(eyr) >= 2020 && parseInt(eyr) <= 2030,
-    "hgt": (hgt) => {
+    byr: (byr: string) =>
+      byr.length === 4 && parseInt(byr) >= 1920 && parseInt(byr) <= 2002,
+    iyr: (iyr: string) =>
+      iyr.length === 4 && parseInt(iyr) >= 2010 && parseInt(iyr) <= 2020,
+    eyr: (eyr: string) =>
+      eyr.length === 4 && parseInt(eyr) >= 2020 && parseInt(eyr) <= 2030,
+    hgt: (hgt: string) => {
       let result = /^(?<num>\d+)(?<msr>[a-z]{2}$)/.exec(hgt);
-      let isValid = false
+      let isValid = false;
       if (result && result.groups) {
-        let { num, msr } = result.groups
+        let { num, msr } = result.groups;
         if (msr === "cm") {
-          isValid = parseInt(num) >= 150 && parseInt(num) <= 193
+          isValid = parseInt(num) >= 150 && parseInt(num) <= 193;
         } else if (msr === "in") {
-          isValid = parseInt(num) >= 59 && parseInt(num) <= 76
+          isValid = parseInt(num) >= 59 && parseInt(num) <= 76;
         }
       }
 
-      return isValid
+      return isValid;
     },
-    "hcl": (hcl) => /^\#[a-f0-9]{6}$/.test(hcl),
-    "ecl": (ecl) => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].indexOf(ecl) > -1,
-    "pid": (pid) => /^[0-9]{9}$/.test(pid)
-  }
+    hcl: (hcl: string) => /^\#[a-f0-9]{6}$/.test(hcl),
+    ecl: (ecl: string) =>
+      ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].indexOf(ecl) > -1,
+    pid: (pid: string) => /^[0-9]{9}$/.test(pid),
+  };
 
   return input.filter((record: { [x: string]: any; cid?: any }) => {
-    let isValid = 
-        Object.keys(record).length === 8 ||
-        (Object.keys(record).length === 7 && record.cid === undefined);
+    let isValid =
+      Object.keys(record).length === 8 ||
+      (Object.keys(record).length === 7 && record.cid === undefined);
 
     for (let key in rules) {
-      isValid = isValid && rules[key](record[key])
+      isValid = isValid && rules[key](record[key]);
     }
-    return isValid
+    return isValid;
   }).length;
 };
-
-/* Tests */
-
-test(1, 1);
 
 /* Results */
 
