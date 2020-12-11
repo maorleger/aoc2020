b540695f68d83df43c8d00c500eaaf6649c5e384
Maor Leger <malege@microsoft.com>
2020-12-11 09:32:00 -0800

    handle missing arguments

--- scripts/start.js
+++ scripts/start.js
@@ -1,15 +1,20 @@
-const { spawn, execSync } = require("child_process")
-const { readdirSync } = require("fs")
-const { cp } = require("shelljs")
+const { spawn, execSync } = require("child_process");
+const { readdirSync } = require("fs");
+const { cp } = require("shelljs");
 
-const day = process.argv[2]
-const days = readdirSync("./src")
+const day = process.argv[2];
+
+if (!day) {
+  throw new Error("Missing day from argument list");
+}
+const days = readdirSync("./src");
 
 if (!days.includes(day)) {
-  console.log(`Creating file structure for ${day}...`)
-  cp("-r", "src/template", `src/${day}`)
+  console.log(`Creating file structure for ${day}...`);
+  cp("-r", "src/template", `src/${day}`);
 }
 
 spawn("nodemon", ["-x", "ts-node", `src/${day}/index.ts`], {
-  stdio: "inherit", shell: true
-})
+  stdio: "inherit",
+  shell: true,
+});
