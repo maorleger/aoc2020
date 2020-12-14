df2fa2d200d42ce585247fdeeb0c4023c044fee4
Maor Leger <maor.leger@gmail.com>
2020-12-14 08:38:28 -0800

    day14 p1

--- src/day13/index.ts
+++ src/day13/index.ts
@@ -1,7 +1,7 @@
-import { test, readInput } from "../utils/index";
+import { readInput } from "../utils/index";
 import { EOL } from "os";
 import * as _ from "lodash";
-import { identity, stubObject } from "lodash";
+import { off } from "process";
 
 const prepareInput = (rawInput: string) => {
   let [targetTime, schedules] = rawInput.split(EOL);
@@ -15,8 +15,8 @@ const input = prepareInput(readInput());
 
 const goA = ({ targetTime, schedules }) => {
   let earliestTimes = schedules
-    .filter((it) => !isNaN(it))
-    .map((id) => {
+    .filter((it: number) => !isNaN(it))
+    .map((id: number) => {
       let departurePrior = Math.floor(targetTime / id) * id;
       if (departurePrior === targetTime) {
         return {
@@ -30,14 +30,35 @@ const goA = ({ targetTime, schedules }) => {
         };
       }
     })
-    .sort((a, b) => a.wait - b.wait);
+    .sort((a: { wait: number }, b: { wait: number }) => a.wait - b.wait);
   return earliestTimes[0].id * earliestTimes[0].wait;
+};
+
+const modularInverse = (a, mod) => {
+  const b = a % mod;
+  for (let i = 1; i < mod; i++) {
+    if ((b * i) % mod === 1) {
+      return i;
+    }
+  }
+  return 1;
+};
+const crt = (congruences) => {
+  const product = congruences.reduce((acc, val) => acc * val[0], 1);
+
+  let sum = 0;
+  for (const [ni, ai] of congruences) {
+    const div = Math.floor(product / ni);
+    sum += ai * modularInverse(div, ni) * div;
+  }
+
+  return sum % product;
 };
 
 // Not my solution
 const goB = ({ schedules }) => {
   let validSchedules = schedules
-    .map((schedule, offset) => ({ schedule, offset }))
+    .map((schedule: number, offset: number) => ({ schedule, offset }))
     .filter(({ schedule }) => !isNaN(schedule));
 
   let i = 1;
@@ -53,6 +74,13 @@ const goB = ({ schedules }) => {
     }
   }
 
+  const congruences = validSchedules.map(({ schedule, offset }) => [
+    schedule,
+    schedule - offset,
+  ]);
+
+  const result = crt(congruences);
+  // return result;
   return time;
 };
 
---
+++ src/day14/index.ts
@@ -0,0 +1,95 @@
+import { test, readInput } from "../utils/index";
+import { EOL } from "os";
+import * as _ from "lodash";
+import { values } from "lodash";
+
+const prepareInput = (rawInput: string) => {
+  let rows = rawInput.split(EOL);
+  let groups = [];
+  let currentMask = "";
+  rows.forEach((row) => {
+    let data = row.split(" = ");
+    if (data[0] === "mask") {
+      groups.push({ kind: "mask", value: data[1].split("") });
+    } else {
+      let address = /^mem\[(?<addr>\d+)\] = (?<val>\d+)$/.exec(row);
+      groups.push({
+        kind: "instruction",
+        addr: parseInt(address.groups.addr)
+          .toString(2)
+          .padStart(36, "0")
+          .split(""),
+        value: parseInt(address.groups.val)
+          .toString(2)
+          .padStart(36, "0")
+          .split(""),
+      });
+    }
+  });
+  return groups;
+};
+
+const input = prepareInput(readInput());
+
+const goA = (input) => {
+  const applyBitmask = (value, bitmask) => {
+    return value.map((v, i) => {
+      if (bitmask[i] === "X") {
+        return v;
+      }
+      return bitmask[i];
+    });
+  };
+  console.log(input);
+  let memory = _.fill(new Array(36), 0);
+  let currentMask = "";
+  input.forEach((row) => {
+    if (row.kind === "mask") {
+      currentMask = row.value;
+    } else {
+      let value = applyBitmask(row.value, currentMask);
+      memory[parseInt(row.addr.join(""), 2)] = parseInt(value.join(""), 2);
+    }
+  });
+  return memory.reduce((acc, n) => acc + n);
+};
+
+const goB = (input) => {
+  const applyBitmask = (value, bitmask) => {
+    return value.map((v, i) => {
+      if (bitmask[i] === "X" || bitmask[i] === "1") {
+        return bitmask[i];
+      }
+      return v;
+    });
+  };
+  let memory = _.fill(new Array(36), _.fill(new Array(36), "0"));
+  console.log(memory);
+
+  let currentMask = "";
+  input.forEach((row) => {
+    if (row.kind === "mask") {
+      currentMask = row.value;
+    } else {
+      let value = applyBitmask(
+        row.addr.toString(2).padStart(36, "0").split(""),
+        currentMask
+      );
+      console.log(value);
+    }
+  });
+};
+
+/* Tests */
+
+// test()
+
+/* Results */
+
+console.time("Time");
+const resultA = goA(input);
+const resultB = goB(input);
+console.timeEnd("Time");
+
+console.log("Solution to part 1:", resultA, 15018100062885);
+console.log("Solution to part 2:", resultB);
